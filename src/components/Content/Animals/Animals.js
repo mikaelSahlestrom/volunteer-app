30690c1e2b2103c57da618883972bb0c9b2cb4e5
kenes <kenes@kth.se>
2021-04-20 18:52:02 +0200

    Add header

--- src/components/Content/Animals/Animals.js
+++ src/components/Content/Animals/Animals.js
@@ -4,7 +4,8 @@ import styles from './Animals.module.css'
 function Animals() {
     return (
         <div className={styles.animals}>
-            All the dogs should have fancy hats 
+            All the dogs should have fancy hats.
+            <h3>Woah! Added new line!</h3>
             <br />
         </div>
     )
