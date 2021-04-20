386e75fecf74bb518c8f6e40643b2a1a3a79e663
sadan <sadan@kth.se>
2021-04-20 18:47:56 +0200

    init schedules

--- src/components/Content/Schedule/Schedule.js
+++ src/components/Content/Schedule/Schedule.js
@@ -4,6 +4,7 @@ function Schedule() {
     return (
         <div className={styles.schedule}>
             Here we should import some calender thingy
+            Things will happen here shortly....
         </div>
     )
 }
