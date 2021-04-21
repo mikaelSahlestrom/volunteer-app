80d929a90f12d7681e80581e0863513981c6e261
kenes <kenes@kth.se>
2021-04-21 03:47:31 +0200

    Update animal

--- src/components/Content/Animals/Animals.js
+++ src/components/Content/Animals/Animals.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import styles from './Animals.module.css'
+import AnimalElem from './AnimalElem'
 
 function Animals() {
     return (
         <div className={styles.animals}>
-            All the dogs should have fancy hats.
-            <h3>Woah! Added new line!</h3>
+            <h2>Animals</h2>
+            <AnimalElem />
+            <AnimalElem />
+            <AnimalElem />
+            <AnimalElem />
+            <AnimalElem />
+            <AnimalElem />
             <br />
         </div>
     )
