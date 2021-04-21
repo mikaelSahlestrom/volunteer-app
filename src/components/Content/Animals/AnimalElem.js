6cf66bf056a0fdc206f29aae8248ec71fb62199a
kenes <kenes@kth.se>
2021-04-21 03:40:58 +0200

    Try React

---
+++ src/components/Content/Animals/AnimalElem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function AnimalElem() {
+    return (
+        <div>
+            <img></img>
+            <h5>Animal</h5>
+            <p>Animal description & stuff</p>
+        </div>
+    )
+}
+export default AnimalElem
