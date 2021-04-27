6b52f8712fa2b0f4c9cdb8cbd4f7302e55bd9319
Adam Ekelöf <adameke@kth.se>
2021-04-27 18:12:42 +0200

    Trying to create mapping

--- src/components/Content/Chat/Chat.js
+++ src/components/Content/Chat/Chat.js
@@ -22,12 +22,55 @@ function Chat() {
             date: new Date(2021, 1, 1, 16, 10)
         },
     ];
+
+    let chatMessages = [];
+
+    let testBotMessages = [
+        {
+            position: 'right',
+            type: 'text',
+            text: 'And that\'s only the first part! Come and look in here.',
+            date: new Date(),
+        },
+        {
+            position: 'left',
+            type: 'text',
+            text: 'Whirr! Whirr!',
+            date: new Date(),
+        },
+        {
+            position: 'right',
+            type: 'text',
+            text: 'I think you\'ll find this present a valuable addition to our modern lifestyle!',
+            date: new Date(),
+        },
+        {
+            position: 'right',
+            type: 'text',
+            text: 'They\'re "Techno Trousers". Ex-NASA. Fantastic for walkies!',
+            date: new Date(),
+        },
+    ]
+
+    let adamMessages = [
+        {
+            position: 'right',
+            type: 'text',
+            text: 'And that\'s only the first part! Come and look in here.',
+            date: new Date(),
+        }
+    ]
     
+    let chatMap = new Map()
+    chatMap.set('TestBot', testBotMessages)
+    chatMap.set('Adam', adamMessages)
+
     const [showMessages, setShowMessages] = useState(false)
     const [showList, setShowList] = useState(true)
 
     const openMessages = (person) => {
         setShowMessages(true)
+        chatMessages = chatMap.get(person.title)
     }
 
     const cancel = () => {
@@ -49,46 +92,32 @@ function Chat() {
     )
     if (showMessages) {
         chatScreen = (
-            <div>
+            <div >
                 <button onClick = {cancel}>
                     Go back
                 </button>
                 <MessageList
-                    className = 'Messages'
+                    className = 'messages'
                     lockable = {true}
                     toBottomHeight = {'100%'}
-                    dataSource = {[
-                        {
-                            position: 'right',
-                            type: 'text',
-                            text: 'Consectetur adipisicing elit',
-                            date: new Date(),
-                        },
-                        {
-                            position: 'right',
-                            type: 'text',
-                            text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
-                            date: new Date(),
-                        },
-                        {
-                            position: 'left',
-                            type: 'text',
-                            text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
-                            date: new Date(),
-                        },
-                        {
-                            position: 'right',
-                            type: 'text',
-                            text: 'Lorem ipsum dolor sit amet',
-                            date: new Date(),
-                        },
-                    ]}
+                    dataSource = {chatMessages}
+                />
+                <Input
+                    classname='messages-input'
+                    placeholder="Type here..."
+                    multiline={true}
+                    rightButtons={
+                        <Button
+                            color='white'
+                            backgroundColor='black'
+                            text='Send'/>
+                    }
                 />
             </div>
         )
     }
     return (
-        <div>
+        <div className='chatscreen'>
             {chatScreen}
         </div>
     )
